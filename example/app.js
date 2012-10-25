5cf36df4e1ecd9262f7026ddb2ecd0101d27eba1
Rodrigo Guerreiro <6speedpt@gmail.com>
2012-10-25 11:15:09 +0100

    awesome feature: view routing based on device type

--- example/app.js
+++ example/app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 app.configure(function(){
     app.set('view engine', 'ejs');
-    app.set('view options', { layout: false });
+    app.set('view options', { layout: true });
     app.set('views', __dirname + '/views');
     
     app.use(express.bodyParser());
@@ -15,12 +15,29 @@ app.configure(function(){
     app.use(device.capture());
     
     app.enableDeviceHelpers();
+    app.enableViewRouting();
 
     app.use(app.router);
 });
 
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Testing express-device' });
+    res.render('index', { title: 'Testing express-device', layout: false });
+});
+
+app.get('/2', function(req, res) {
+    res.render('index2', { title: 'Testing express-device with layout' });
+});
+
+app.get('/3', function(req, res) {
+    res.render('index2', { title: 'Testing express-device with layout', layout: 'xpto' });
+});
+
+app.get('/4', function(req, res) {
+    res.render('index2', { title: 'Testing express-device with layout', layout: 'etc/layout' });
+});
+
+app.get('/5', function(req, res) {
+    res.render('index3', { title: 'Testing express-device with layout' });
 });
 
 app.listen(port);
