67735be88eca0be29cfa8aaefdf80226e83b1743
Rodrigo Guerreiro <6speedpt@gmail.com>
2013-01-25 17:52:56 +0000

    added config options as suggested in #9

---
+++ test/not_default_options_test.js
@@ -0,0 +1,88 @@
+var device = require('../lib/device.js'),
+    assert = require('assert');
+
+describe('device', function() {
+    describe('unknown user-agent',function(){
+        it('should get phone (default one)', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': 'dsfglkfjawflkehf' } });
+            assert.equal(parser.get_type(), 'phone');
+        });
+    });
+
+    describe('unknown user-agent configured to return desktop',function(){
+        it('should get desktop', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': 'dsfglkfjawflkehf' } }, { deviceTypeForUnkownUserAgent: 'desktop' });
+            assert.equal(parser.get_type(), 'desktop');
+        });
+    });
+
+    describe('unknown user-agent configured to return tablet',function(){
+        it('should get tablet', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': 'dsfglkfjawflkehf' } }, { deviceTypeForUnkownUserAgent: 'tablet' });
+            assert.equal(parser.get_type(), 'tablet');
+        });
+    });
+
+    describe('unknown user-agent configured to return bot',function(){
+        it('should get bot', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': 'dsfglkfjawflkehf' } }, { deviceTypeForUnkownUserAgent: 'bot' });
+            assert.equal(parser.get_type(), 'bot');
+        });
+    });
+
+    describe('unknown user-agent configured to return tv',function(){
+        it('should get tv', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': 'dsfglkfjawflkehf' } }, { deviceTypeForUnkownUserAgent: 'tv' });
+            assert.equal(parser.get_type(), 'tv');
+        });
+    });
+
+    describe('with no user-agent',function(){
+        it('should get desktop', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': '' } });
+            assert.equal(parser.get_type(), 'desktop');
+        });
+    });
+
+    describe('with no user-agent',function(){
+        it('should get desktop', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': null } });
+            assert.equal(parser.get_type(), 'desktop');
+        });
+    });
+
+    describe('with no user-agent',function(){
+        it('should get desktop', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': undefined } });
+            assert.equal(parser.get_type(), 'desktop');
+        });
+    });
+
+    describe('with no user-agent and configured to return phone',function(){
+        it('should get phone', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': null } }, { defaultDeviceType: 'phone' });
+            assert.equal(parser.get_type(), 'phone');
+        });
+    });
+
+    describe('with no user-agent and configured to return tablet',function(){
+        it('should get tablet', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': null } }, { defaultDeviceType: 'tablet' });
+            assert.equal(parser.get_type(), 'tablet');
+        });
+    });
+
+    describe('with no user-agent and configured to return tv',function(){
+        it('should get tv', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': null } }, { defaultDeviceType: 'tv' });
+            assert.equal(parser.get_type(), 'tv');
+        });
+    });
+
+    describe('with no user-agent and configured to return bot',function(){
+        it('should get bot', function(){
+            var parser = new device.Parser({ headers: { 'user-agent': null } }, { defaultDeviceType: 'bot' });
+            assert.equal(parser.get_type(), 'bot');
+        });
+    });
+});
