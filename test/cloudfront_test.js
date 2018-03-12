0a574344448fce21fa79ba44599395ce47fcc93e
lucianohgo <luciano.hg4@gmail.com>
2018-03-12 16:06:29 -0300

    Checking for Amazon Cloudfront user-agent
    Instead of removing the user agent, CloudFront resets it to
    `Amazon CloudFront`. This behavior can be test within a container
    running behind Cloudfront by instrumenting the device.js file and
    logging the headers:
    ```yml
    user-agent: Amazon CloudFront
    req[headers]: {
      ...
      'cloudfront-is-desktop-viewer': 'true',
      'cloudfront-is-mobile-viewer': 'false',
      'cloudfront-is-tablet-viewer': 'false',
       ....
      'user-agent': 'Amazon CloudFront',
      ...
      connection: 'close' }
    ```
    
    See [CloudFront User-Agent docs](https://goo.gl/S5ojzM)

--- test/cloudfront_test.js
+++ test/cloudfront_test.js
@@ -1,22 +1,76 @@
-var device = require('../lib/device.js'),
-    assert = require('assert');
+const device = require('device');
+const expressDevice = require('../lib/device.js');
+const assert = require('assert');
 
 describe('cloudfront', function() {
-    describe('with desktop header',function(){
-        it('should get device type desktop', function(){
-            var type = device.customCheck({ headers: { 'cloudfront-is-desktop-viewer': 'true' } });
+    let headers = {};
+
+    describe('with user-agent is present', () => {
+        const mockUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) Chrome/65.0.3325.146';
+        beforeEach(() => { headers = { 'user-agent': mockUserAgent }; });
+        afterEach(() => { headers = {}; });
+
+        describe('with cloudfront user agent', () => {
+            beforeEach(() => { headers = { ...headers, 'user-agent': 'Amazon CloudFront' }; });
+            afterEach(() => { headers = {}; });
+            
+            describe('with desktop viewer', () => {
+                beforeEach(() => { headers = { ...headers, 'cloudfront-is-desktop-viewer': 'true' } });
+
+                it('should get device type desktop', () => {
+                    const type = expressDevice.customCheck({ headers })
+                    assert.equal(type, 'desktop');
+                });
+            });
+            describe('with mobile viewer', () => {
+                beforeEach(() => { headers = { ...headers, 'cloudfront-is-mobile-viewer': 'true' } });
+
+                it('should get device type phone', function () {
+                    const type = expressDevice.customCheck({ headers })
+                    assert.equal(type, 'phone');
+                });
+            });
+            describe('with tablet viewer', () => {
+                beforeEach(() => { headers = { ...headers, 'cloudfront-is-tablet-viewer': 'true' } });
+
+                it('should get device type tablet', function () {
+                    const type = expressDevice.customCheck({ headers })
+                    assert.equal(type, 'tablet');
+                });
+            });
+        });
+
+        it('should return the present device type', () => {
+            const mydevice = device(mockUserAgent, {});
+            const type = expressDevice.customCheck({ headers: { ...headers, 'cloudfront-is-phone-viewer': 'true' } }, mydevice);
+            assert.equal(type, mydevice.type);
+        });
+    });
+
+    describe('with desktop viewer', () => {
+        beforeEach(() => { headers = { ...headers, 'cloudfront-is-desktop-viewer': 'true' } });
+        afterEach(() => { headers = {}; });
+
+        it('should get device type desktop', () => {
+            const type = expressDevice.customCheck({ headers })
             assert.equal(type, 'desktop');
         });
     });
-    describe('with mobile viewer', function () {
+    describe('with mobile viewer', () => {
+        beforeEach(() => { headers = { ...headers, 'cloudfront-is-mobile-viewer': 'true' } });
+        afterEach(() => { headers = {}; });
+
         it('should get device type phone', function () {
-            var type = device.customCheck({ headers: { 'cloudfront-is-mobile-viewer': 'true' } });
+            const type = expressDevice.customCheck({ headers })
             assert.equal(type, 'phone');
         });
     });
-    describe('with tablet viewer', function () {
+    describe('with tablet viewer', () => {
+        beforeEach(() => { headers = { ...headers, 'cloudfront-is-tablet-viewer': 'true' } });
+        afterEach(() => { headers = {}; });
+
         it('should get device type tablet', function () {
-            var type = device.customCheck({ headers: { 'cloudfront-is-tablet-viewer': 'true' } });
+            const type = expressDevice.customCheck({ headers })
             assert.equal(type, 'tablet');
         });
     });
